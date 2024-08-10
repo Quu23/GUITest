6796d6d546f4ce381f7a35fdaa8900d5ac32adf9
Quu23 <kiyu@kaz.bbiq.jp>
2024-08-10 21:51:13 +0900

    プロジェクト ファイルを追加します。

---
+++ GUITest/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GUITest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Paint_1(object sender, PaintEventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            Bitmap p = new Bitmap(1, 1);
+            p.SetPixel(0, 0, Color.Black);
+            g.DrawImageUnscaled(p, 100, 100);
+            g.FillEllipse(Brushes.Black, 100, 100,100,100);
+        }
+    }
+}
