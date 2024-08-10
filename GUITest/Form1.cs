45511af2a1e16a517a0a1a0b5f969a051a95e9a3
Quu23 <kiyu@kaz.bbiq.jp>
2024-08-10 23:00:44 +0900

    よくわからんけどtimerいれてみた

--- GUITest/Form1.cs
+++ GUITest/Form1.cs
@@ -12,6 +12,8 @@ namespace GUITest
 {
     public partial class Form1 : Form
     {
+        public int x = 0;
+
         public Form1()
         {
             InitializeComponent();
@@ -21,18 +23,13 @@ namespace GUITest
         private void Form1_Paint_1(object sender, PaintEventArgs e)
         {
             Graphics g = this.CreateGraphics();
-            Bitmap p = new Bitmap(1, 1);
-            p.SetPixel(0, 0, Color.Black);
-            g.DrawImageUnscaled(p, 100, 100);
-            g.FillRectangle(Brushes.Black, 100, 100,100,100);
-            g.FillRectangle(Brushes.Cyan, 250, 100, 100, 100);
-            g.FillRectangle(Brushes.Yellow, 100, 250, 100, 100);
-            g.FillRectangle(Brushes.Red, 500, 100, 150, 100);
-            g.FillRectangle(Brushes.Blue, 500, 250, 100, 150);
-            g.FillRectangle(Brushes.OrangeRed, 100, 100, 100, 100);
-
-
-
+            //四角形ひょうじ
+            g.FillRectangle(Brushes.Black    , x, 100, 100, 100);
+            //g.FillRectangle(Brushes.Cyan     , 250, 100, 100, 100);
+            //g.FillRectangle(Brushes.Yellow   , 100, 250, 100, 100);
+            //g.FillRectangle(Brushes.Red      , 500, 100, 150, 100);
+            //g.FillRectangle(Brushes.Blue     , 500, 250, 100, 150);
+            //g.FillRectangle(Brushes.OrangeRed, 100, 100, 100, 100);
 
 
         }
--- GUITest/Program.cs
+++ GUITest/Program.cs
@@ -8,15 +8,38 @@ namespace GUITest
 {
     internal static class Program
     {
+        static Form1 form;
+        static Timer timer;
+
         /// <summary>
         /// アプリケーションのメイン エントリ ポイントです。
         /// </summary>
         [STAThread]
         static void Main()
         {
+            Console.WriteLine("aaa");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            form = new Form1();
+            Application.Run(form);
+
+            timer = new Timer();
+            timer.Tick += new EventHandler(loop);
+            timer.Interval = 1000;
+
+            timer.Start();
+            Console.WriteLine("kajkaj");
+
+            Application.DoEvents();
+
+        }
+
+        public static void loop(Object myObject,EventArgs myEventArgs)
+        {
+            form.Refresh();
+            form.x += 10;
+            Console.WriteLine(form.x);
+            if (form.x > 500) timer.Stop();
         }
     }
 }
